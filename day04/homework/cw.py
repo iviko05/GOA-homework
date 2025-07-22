b55ac47eb6543c0fa36b809fad8a2cad968ad0ce
iviko05 <ivikotodua1@gmail.com>
2025-07-22 19:43:26 +0400

    1

---
+++ day04/homework/cw.py
@@ -0,0 +1,60 @@
+# task2
+# input - როცა მომხმარებელს შემოაქვს ისა თუის ინფორმაცია
+# output -როცა მომხმარებლისთვის გამოგვაქვს ესა Tუ ის ინფორმაცია
+
+name = input("enter your name: ")
+
+print(name)
+
+# task3
+name = input("enter your name: ")
+
+print(type(name))
+
+# task4
+str1 = "luka"   # string
+str2 = "lia"     # string
+str3 = "manqana"    # string
+str4 = "nia"    # string
+str5 = "salome" # string
+
+int1 = 30   # integer
+int2 = 20   #integer
+int3 = 10   #integer
+int4 = 60   #integer
+int5 = 50   #integer
+
+float1 = 5.2    #float
+float2 = 3.2    #foat
+float3 = 6.5    #float
+float4 = 2.2    #float
+float5 = 4.4    #float
+
+
+
+# task5
+print(type(str4))
+print(type(int3))
+print(type(float3))
+
+#task6
+print("iviko" + "todua" + "15")
+
+name =input("enter your name: ")
+last_name = input("enter your last name: ")
+print(name + last_name)
+
+# task7
+num1 = int(input("enter first number"))
+num2 = int(input("enter your second number"))
+num3 = int(input("enter your third number"))
+num4 = int(input("enter your fourth number"))
+num5 = int(input("entr your fiveth number"))
+print((num1+num2+num3+num4+num5)/5)
+
+# task8
+name1 = input("enter your name")
+name2 = input("enter your last name")
+age3 = input("enter your age")
+weight4 = input("enter your weight")
+print(name1+" "+name2+" "+age3+" "+weight4)
--- day05/classwork/cw.py
+++ day05/classwork/cw.py
@@ -1,6 +1,6 @@
 num1=int (input())
 
 num2=int (input())
-print(num==num2)
+print(num1==num2)
 print(num1+num2)
-print(num1-num2)
+print(num1-num1)

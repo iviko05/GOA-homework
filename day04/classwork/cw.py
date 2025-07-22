258336c0fb89750fec51e2ed661484a4a88a67c3
iviko05 <ivikotodua1@gmail.com>
2025-07-22 18:32:24 +0400

    1

---
+++ day04/classwork/cw.py
@@ -0,0 +1,42 @@
+# task1
+
+name = input("enter your name: ")
+print(name)
+
+
+# task2
+number1 = input("enter your first number: ")
+
+number2 = input("enter your second number: ")
+number1 = int(number1)
+number2 = int(number2)
+print(number1+number2)
+
+
+# task3
+age = input("enter your age: ")
+print(type(age))
+age = int(age)
+
+print(age)
+
+print(type(age))
+
+
+# task4
+
+last_name = "todua"
+
+age = 19
+
+weight = 6.5
+
+boolean = True
+
+print(type(last_name))
+
+print(type(age))
+
+print(type(weight))
+
+print(type(boolean))
